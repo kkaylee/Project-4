bd12506365f800f862c3947cb59caf431789c439
kkaylee <kaylee.kappes@patelhs.org>
2021-02-03 14:04:00 +0000

    I added the method and received the name, lowest grade, ad highest grade.

--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
-
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String args[]) {
-    // creating Scanner class object
+  public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    System.out.print("Enter the number: ");
-   
-      System.out.println(num + " is even");
-    else
-      System.out.println(num + " is odd");
+    System.out.println("What is your name?");
+    String name = scan.next();
+    System.out.println("What was your highest grade in class?");
+    int highestGrade = scan.nextInt();
+    System.out.println("What is your lowest grade in class.");
+    int lowestGrade = scan.nextInt();
   }
-}
+
+  // method calculates ave
+  static int aveGrade(int lowestGrade, int highestGrade) {
+    int grade = lowestGrade + highestGrade;
+    int ave = grade / 2;
+    return ave;
+  
+    }
+  }
